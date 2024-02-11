1b8d2b6490268a216ff1ce3a6b010f9b6cad0055
Ethan Greeff <greeff@mercury.cs.colostate.edu>
2024-02-11 09:44:53 -0700

    add Place class

---
+++ server/src/main/java/com/tco/requests/Place.java
@@ -0,0 +1,9 @@
+package com.tco.requests;
+
+import com.tco.misc.GeographicCoordinate;
+
+import java.util.LinkedHashMap;
+
+class Place extends LinkedHashMap<String,String> implements GeographicCoordinate {
+    
+}

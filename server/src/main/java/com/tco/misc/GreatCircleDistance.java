64687ac7d9891d6c1c174c9d88478fee9e83c45d
Riley Radici <rileyr3@colostate.edu>
2024-02-12 15:03:40 -0700

    Update return value

--- server/src/main/java/com/tco/misc/GreatCircleDistance.java
+++ server/src/main/java/com/tco/misc/GreatCircleDistance.java
@@ -1,5 +1,5 @@
 package com.tco.misc;
 
 public interface GreatCircleDistance extends GeographicCoordinate {
-    public double between(GeographicCoordinate from, GeographicCoordinate to, double earthRadius);
+    public Long between(GeographicCoordinate from, GeographicCoordinate to, double earthRadius);
 }

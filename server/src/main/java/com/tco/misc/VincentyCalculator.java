f81772b6a44c9b49f968563bfc02a9db85045db7
Zavion Madison <Zjp32324@colostate.edu>
2024-02-13 10:47:06 -0700

    vincenty class with between

--- server/src/main/java/com/tco/misc/VincentyCalculator.java
+++ server/src/main/java/com/tco/misc/VincentyCalculator.java
@@ -1,5 +1,7 @@
 package com.tco.misc;
 
 public class VincentyCalculator implements GreatCircleDistance{
-    
+    public Long between(GeographicCoordinate from, GeographicCoordinate to, double earthRadius){
+        return 0L;
+    }
 }

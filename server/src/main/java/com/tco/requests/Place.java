f54c63e1a862e18f9421dbf68e92714bc6773afe
gknudtson <gknudt@colostate.edu>
2024-02-13 14:45:07 -0700

    add constructor

--- server/src/main/java/com/tco/requests/Place.java
+++ server/src/main/java/com/tco/requests/Place.java
@@ -5,6 +5,10 @@ import com.tco.misc.GeographicCoordinate;
 import java.util.LinkedHashMap;
 
 class Place extends LinkedHashMap<String,String> implements GeographicCoordinate {
+    public Place(double longitude, double latitude) {
+        this.longitude = longitude;
+        this.latitude = latitude;
+    }
     
     public Double lonRadians() { return 0.0; }
     public Double latRadians() { return 0.0; }

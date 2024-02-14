f6b4ec1a1044284791e424a5b0fb4483eecbf73b
gknudtson <gknudt@colostate.edu>
2024-02-14 09:11:19 -0700

    add CosineCalculator class

---
+++ server/src/main/java/com/tco/misc/CosineCalculator.java
@@ -0,0 +1,13 @@
+package com.tco.misc;
+
+public class CosineCalculator implements GreatCircleDistance {
+
+    public Long between(
+        GeographicCoordinate from,
+        GeographicCoordinate to,
+        double earthRadius) {
+
+            return 0L;
+        }
+    
+}

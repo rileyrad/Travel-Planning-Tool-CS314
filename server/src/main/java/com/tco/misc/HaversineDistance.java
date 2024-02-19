4fd37da8cf141930b27b531f5e9e0ec5a08d3d37
Ethan Greeff <greeff@salt-lake-city.cs.colostate.edu>
2024-02-19 12:40:39 -0700

    add helper functions to HaversineDistance

--- server/src/main/java/com/tco/misc/HaversineDistance.java
+++ server/src/main/java/com/tco/misc/HaversineDistance.java
@@ -5,5 +5,17 @@ public class HaversineDistance implements GreatCircleDistance {
     
     public Long between(GeographicCoordinate from, GeographicCoordinate to, double earthRadius) {
             return 0L;
-        }
+    }
+
+    private Double sinSquaredOfDifferenceHalved(Double subtractFrom, Double subtract){
+        Double difference = subtractFrom - subtract;
+        Double differenceHalved = difference/2;
+        return Math.sin(differenceHalved) * Math.sin(differenceHalved);
+    }
+
+    private Double cosSquaredOfSumHalved(Double val1, Double val2){
+        Double sum = val1 + val2;
+        Double sumHalved = sum/2;
+        return Math.cos(sumHalved) + Math.cos(sumHalved);
+    }
 }

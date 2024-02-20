ead20191b58d9731a951a7ac9ba65b5d2c391695
gknudtson <gknudt@colostate.edu>
2024-02-20 11:16:45 -0700

    implement between method in cosines

--- server/src/main/java/com/tco/misc/CosinesDistance.java
+++ server/src/main/java/com/tco/misc/CosinesDistance.java
@@ -2,13 +2,18 @@ package com.tco.misc;
 
 public class CosinesDistance implements GreatCircleDistance {
     public CosinesDistance(){};
-    
-    public Long between(
-        GeographicCoordinate from,
-        GeographicCoordinate to,
-        double earthRadius) {
 
-            return 0L;
-        }
-    
+    public Long between(GeographicCoordinate from, GeographicCoordinate to, double earthRadius) {
+        double from_lon = from.lonRadians();
+        double from_lat = from.latRadians();
+        double to_lon = to.lonRadians();
+        double to_lat = to.latRadians();
+
+        Long distance = earthRadius * Math.acos(Math.sin(from_lat) * Math.sin(to_lat) + 
+                Math.cos(from_lat) * Math.cos(to_lat) * Math.cos(from_lon - to_lon));
+                
+        return distance;
+    }
+
+
 }

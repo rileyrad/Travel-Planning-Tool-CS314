2012c93ab427c0c38f882a4cf3ad036e3f4e7529
Ethan Greeff <greeff@salt-lake-city.cs.colostate.edu>
2024-02-20 16:34:01 -0700

    add constructor to vincenty

--- server/src/main/java/com/tco/misc/VincentyDistance.java
+++ server/src/main/java/com/tco/misc/VincentyDistance.java
@@ -1,6 +1,9 @@
 package com.tco.misc;
 
 public class VincentyDistance implements GreatCircleDistance{
+    
+    public VincentyDistance() {}
+    
     public Long between(GeographicCoordinate from, GeographicCoordinate to, double earthRadius){
         return 0L;
     }

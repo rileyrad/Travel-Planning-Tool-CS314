29bd465bf9b973406a04fffb47984b5134bc4a3f
gknudtson <gknudt@colostate.edu>
2024-03-05 11:38:22 -0700

    define get method

--- server/src/main/java/com/tco/misc/CalculatorFactory.java
+++ server/src/main/java/com/tco/misc/CalculatorFactory.java
@@ -3,5 +3,8 @@ package com.tco.misc;
 public abstract class CalculatorFactory {
 
     abstract public DistanceCalculator create();
+    static public DistanceCalculator get(String formula){
+        return null;
+    }
 
 }

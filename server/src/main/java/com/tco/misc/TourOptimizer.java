c3422464c3c3dc393ece2992687e918e997a6d1b
Brenner Lattin <brenner.lattin@gmail.com>
2024-03-07 14:57:46 -0700

    Add initialization methods to TourOptimizer

--- server/src/main/java/com/tco/misc/TourOptimizer.java
+++ server/src/main/java/com/tco/misc/TourOptimizer.java
@@ -4,6 +4,37 @@ import com.tco.requests.Place;
 import com.tco.requests.Places;
 
 public abstract class TourOptimizer {
+
     public void improve() {}
+
+    private int[] initializeTour(Places places) {
+        int[] tour = new int[places.size()];
+
+        for (int i = 0; i < places.size(); i++) {
+            tour[i] = i;
+        }
+
+        return tour;
+    }
+
+    private long[][] initializeDistances(Places places, String formula, double radius) {
+        long[][] distances = new long[places.size()][places.size()];
+
+        DistanceCalculator calculator = CalculatorFactory.get(formula);
+
+        for (int fromIndex = 0; fromIndex < places.size() - 1; fromIndex++) {
+            for (int toIndex = fromIndex + 1; toIndex < places.size(); toIndex++) {
+                Place fromPlace = places.get(fromIndex);
+                Place toPlace = places.get(toIndex);
+
+                long distance = calculator.between(fromPlace, toPlace, radius);
+
+                distances[fromIndex][toIndex] = distance;
+                distances[toIndex][fromIndex] = distance;
+            }
+        }
+
+        return distances;
+    }
     
 }

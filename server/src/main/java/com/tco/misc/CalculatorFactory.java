79adb2d96f8c6cda29f743f32e6b6f4c3b7f414c
Ethan Greeff <greeff@mercury.cs.colostate.edu>
2024-03-06 20:16:09 -0700

    implement get in CalculatorFactory

--- server/src/main/java/com/tco/misc/CalculatorFactory.java
+++ server/src/main/java/com/tco/misc/CalculatorFactory.java
@@ -1,10 +1,20 @@
 package com.tco.misc;
 
-public abstract class CalculatorFactory {
+public abstract class CalculatorFactory{
 
     abstract public DistanceCalculator create();
+    
     static public DistanceCalculator get(String formula){
-        return null;
+        if("haversine".equals(formula)){
+            HaversineCreator haversine = new HaversineCreator();
+            return haversine.create();
+        } else if("cosines".equals(formula)){
+            CosinesCreator cosines = new CosinesCreator();
+            return cosines.create();
+        } else{
+            VincentyCreator vincenty = new VincentyCreator();
+            return vincenty.create();
+        }
     }
 
 }

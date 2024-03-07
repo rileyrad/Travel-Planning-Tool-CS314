b8c5ea919994e30c80ad746368bb1297617201f8
gknudtson <gknudt@colostate.edu>
2024-03-07 14:19:40 -0700

    define construct

--- server/src/main/java/com/tco/misc/TourOptimizer.java
+++ server/src/main/java/com/tco/misc/TourOptimizer.java
@@ -4,6 +4,9 @@ import com.tco.requests.Place;
 import com.tco.requests.Places;
 
 public abstract class TourOptimizer {
+    public Places construct(Places places, double radius, String formula, double response) {
+        return null;
+    }
     public void improve() {}
     
 }

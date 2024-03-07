33d7813340da227d24c108e92e09be2a951d0b8a
Zavion Madison <Zjp32324@colostate.edu>
2024-03-07 11:43:28 -0700

    Add imports to TourRequest

--- server/src/main/java/com/tco/requests/TourRequest.java
+++ server/src/main/java/com/tco/requests/TourRequest.java
@@ -1,5 +1,15 @@
 package com.tco.requests;
 
+import com.tco.misc.OptimizerFactory;
+import com.tco.misc.NoOptimizer;
+import com.tco.misc.OneOptimizer;
+import com.tco.misc.TwoOptimizer;
+import com.tco.misc.ThreeOptimizer;
+import com.tco.misc.TourOptimizer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class TourRequest extends Request {
     
     private Places places;

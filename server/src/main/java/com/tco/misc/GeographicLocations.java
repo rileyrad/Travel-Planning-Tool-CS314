8118bf89cd575aaf5854ac794035f120aa800da2
gknudtson <gknudt@colostate.edu>
2024-03-29 15:37:46 -0600

    define find

--- server/src/main/java/com/tco/misc/GeographicLocations.java
+++ server/src/main/java/com/tco/misc/GeographicLocations.java
@@ -4,4 +4,8 @@ import com.tco.requests.Places;
 import java.util.List;
 
 public class GeographicLocations {
+    public Places find(String match, List<String> type, List<String> where, Integer limit, Integer found, Places places) {
+        return null;
+    }
+    
 }

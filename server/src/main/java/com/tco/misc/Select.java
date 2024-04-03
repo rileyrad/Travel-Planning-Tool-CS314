e0af6be68ee4bd428fcc88edb53156a11db2b00e
Brenner Lattin <brenner.lattin@gmail.com>
2024-04-03 15:05:42 -0600

    Add method for finding matches to Select

--- server/src/main/java/com/tco/misc/Select.java
+++ server/src/main/java/com/tco/misc/Select.java
@@ -1,5 +1,25 @@
 package com.tco.misc;
 
 public class Select {
-    
+    private static final String COLUMNS = "world.name";
+
+    static String match(String match, int limit) {
+        match = "%" + match + "%";
+        String where = " WHERE world.name LIKE " + match
+                    + " OR region.name LIKE " + match
+                    + " OR municipality.name LIKE " + match
+                    + " OR country.name LIKE " + match;
+        return statement(where, " LIMIT " + limit);
+    }
+
+    static String statement(String where, String limit) {
+        return "SELECT " + COLUMNS
+                + " FROM world "
+                + " INNER JOIN continent ON world.continent = continent.id"
+                + " INNER JOIN country ON world.iso_country = country.id"
+                + " INNER JOIN region ON world.iso_region = region.id"
+                + where
+                + limit
+                + " ;";
+    }
 }

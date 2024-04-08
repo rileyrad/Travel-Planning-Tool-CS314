a3e3c573f9c102568d9133e810de125fc8a97150
Riley Radici <rileyr3@colostate.edu>
2024-04-08 15:17:37 -0600

    Implemented found

--- server/src/main/java/com/tco/misc/GeographicLocations.java
+++ server/src/main/java/com/tco/misc/GeographicLocations.java
@@ -8,21 +8,33 @@ import java.sql.Connection;
 import java.sql.Statement;
 import java.sql.ResultSet;
 import java.sql.DriverManager;
+import java.lang.Exception;
 
 public class GeographicLocations {
     public Places find(String match, List<String> type, List<String> where, Integer limit, Integer found, Places places) {
         return null;
     }
     
-    public Places near(Place place, Long distance, Long earthRadius, Long limit){
+    public Places near(Place place, Long distance, Long earthRadius, Long limit) {
         return null;
     }
 
-    public Distances distances(){
+    public Distances distances() {
         return null;
     }
-    public Integer found(){
-        return null;
+
+    public Integer found(String match) throw Exception {
+        String sql = Select.found(match);
+		try (
+            // connect to the database and query
+            Connection conn = DriverManager.getConnection(Credential.url(), Credential.USER, Credential.PASSWORD);
+            Statement query = conn.createStatement();
+            ResultSet results = query.executeQuery(sql)
+		) {
+			return count(results);
+		} catch (Exception e) {
+			throw e;
+		}
     }
 
     private double getLongitudeOffset(Place place, double distance, long earthRadius) {

5d737c0c1adbacfaff384326be5d144d48ce14c0
Brenner Lattin <brenner.lattin@gmail.com>
2024-04-19 15:06:17 -0600

    Add method for testing if two opt swap improves tour

--- server/src/main/java/com/tco/misc/TwoOptimizer.java
+++ server/src/main/java/com/tco/misc/TwoOptimizer.java
@@ -5,6 +5,10 @@ public class TwoOptimizer extends TourConstruction {
     public void improve() {
     }
 
+    private boolean swapImproves(int i, int k) {
+        return getDistance(i, k) + getDistance(i + 1, k + 1) < getDistance(i, i + 1) + getDistance(k, k + 1);
+    }
+
     private void twoOptSwap(int i, int k) {
         while (i < k) {
             swapElements(i, k);
